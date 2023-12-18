3d1618d59974259f7782d6a3e4b5b0c373e377d6
Kevin Ruiz <kleonardoruizflorez@gmail.com>
2023-12-18 06:55:02 -0500

    fix: add correct version of mockito

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.assertj:assertj-core:3.11.1")
-    testImplementation("org.mockito:mockito-core:2.1.0")
+    testImplementation("org.mockito:mockito-core:3.+")
 }
 
 tasks.test {
